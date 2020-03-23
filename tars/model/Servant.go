5b5860f4ce5436a2bf340d24701ce8d84d1afb6d
rex <ttt@us.com>
2020-03-23 20:34:51 +0800

    hahah

--- tars/model/Servant.go
+++ tars/model/Servant.go
@@ -11,6 +11,8 @@ type Servant interface {
 	Tars_invoke(ctx context.Context, ctype byte,
 		sFuncName string,
 		buf []byte,
+		status map[string]string,
+		context map[string]string,
 		Resp *requestf.ResponsePacket) error
 
 	TarsSetTimeout(t int)
