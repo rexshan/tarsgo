610f38e7120133789c30561d5f45966a4a4a3995
rex <ttt@us.com>
2020-03-30 16:12:51 +0800

    new proto

--- tars/plugin/tcpToRpc/transfer.go
+++ tars/plugin/tcpToRpc/transfer.go
@@ -18,14 +18,28 @@ type (
 		Encode(output RPCOutput) (body json.RawMessage, err error)
 	}
 
+	ToInvoker interface {
+		Decode(body json.RawMessage) (input MSGInput, err error)
+		Invoke(input MSGInput,Srv,Func string)(output MSGOutput,err error)
+		Encode(output MSGOutput) (body json.RawMessage, err error)
+	}
+
+
 	TCPToRPC struct {
 		comm *tars.Communicator
 	}
 
-	WebsocketToRPC struct {
+	/*WebsocketToRPC struct {
+		comm *tars.Communicator
+	}
+	*/
+
+	MSGToRPC struct {
 		comm *tars.Communicator
 	}
 )
+
+
 
 func invoke(comm *tars.Communicator, input RPCInput) (out RPCOutput, err error) {
 	var (
@@ -88,20 +102,61 @@ func (t *TCPToRPC) Encode(output RPCOutput) (body json.RawMessage, err error) {
 	return
 }
 
-func NewWebsocketToRPC(c *tars.Communicator) RPCInvoker {
-	return &WebsocketToRPC{comm: c}
+/*----------------------------MSGToRPC-----------------------------------------*/
+func NewMsgToRPC(c *tars.Communicator)ToInvoker {
+	return &MSGToRPC{comm:c}
 }
 
-func (w *WebsocketToRPC) Decode(body json.RawMessage) (input RPCInput, err error) {
-	return input, nil
+func (m *MSGToRPC)Decode(body json.RawMessage)(input MSGInput,err error) {
+	if err = pb.Unmarshal(body, &input); err != nil {
+		tars.TLOG.Errorf("MSGInput decode err :%+v", err)
+		return
+	}
+	tars.TLOG.Debugf("MSGInput decode %s %s %+v", input.Alias, input.Opt)
+	return
 }
 
-func (w *WebsocketToRPC) Invoke(input RPCInput) (output RPCOutput, err error) {
-	return output, nil
+func (m *MSGToRPC)Invoke(input MSGInput,Srv,Func string)(output MSGOutput,err error) {
+	start := time.Now().UnixNano() / int64(time.Millisecond)
+	out, err := m.invokeWidth(m.comm, input,Srv,Func)
+	end := time.Now().UnixNano() / int64(time.Millisecond)
+	tars.TLOG.Infof("MSGInput exec time :%d %s %s %+v", int64(end-start), input.Obj, input.Func, input.Opt)
+	return out, err
 }
 
-func (w *WebsocketToRPC) Encode(output RPCOutput) (body json.RawMessage, err error) {
-	return nil, nil
+func (m *MSGToRPC)Encode(output MSGOutput)(body json.RawMessage,err error) {
+	tars.TLOG.Debugf("MSGOutput encode:%d %s %s %+v", output.Ret, output.Desc, output.Opt)
+	if body, err = pb.Marshal(&output); err != nil {
+		tars.TLOG.Errorf("MSGOutput encode error :%+v", err)
+		return
+	}
+	return
+}
+
+func (m *MSGToRPC)invokeWidth(comm *tars.Communicator,input MSGInput,Obj,Func string)(out MSGOutput,err error) {
+	var (
+		rpcStub *tars.ServantProxy
+		_status map[string]string
+	)
+	rpcStub = comm.GetServantProxy(Obj)
+	_resp := new(requestf.ResponsePacket)
+	err = rpcStub.Tars_invoke(context.Background(), 0, LcFirst(Func), input.Req, _status, input.Opt, _resp)
+	if err != nil {
+		tars.TLOG.Errorf("rpc invoke err :%s  %s %+v  %+v", Obj, Func, input.Opt,err)
+		nErr := Parse(err.Error())
+		out.Ret = nErr.Code
+		if nErr.Code == Failure {
+			out.Desc = "网络繁忙，请稍后再试"
+		} else {
+			out.Desc = nErr.Detail
+		}
+		return
+	}
+	out.Ret = _resp.IRet
+	out.Desc = _resp.SResultDesc
+	out.Rsp = tools.Int8ToByte(_resp.SBuffer)
+	out.Opt = _resp.Context
+	return
 }
 
 func LcFirst(str string) string {
